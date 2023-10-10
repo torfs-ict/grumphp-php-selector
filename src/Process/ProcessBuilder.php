1e5467e28d17e1323bca88447502c34d264ae48b
Kristof Torfs <kristof@torfs.org>
2023-10-10 10:07:47 +0200

    Only prepend executable for actual PHP scripts

--- src/Process/ProcessBuilder.php
+++ src/Process/ProcessBuilder.php
@@ -8,6 +8,7 @@ use GrumPHP\Collection\ProcessArgumentsCollection;
 use GrumPHP\Configuration\Model\ProcessConfig;
 use GrumPHP\IO\IOInterface;
 use GrumPHP\Locator\ExternalCommand;
+use PhpParser\ParserFactory;
 use Symfony\Component\Process\Process;
 use TorfsICT\GrumPHP\PhpSelector\PhpSelectorReader;
 
@@ -28,9 +29,17 @@ class ProcessBuilder extends \GrumPHP\Process\ProcessBuilder
     public function buildProcess(ProcessArgumentsCollection $arguments): Process
     {
         if (null !== $this->executable) {
-            $array = $arguments->toArray();
-            array_unshift($array, $this->executable);
-            $arguments = new ProcessArgumentsCollection($array);
+            // Check if the original executable is effectively a PHP script
+            $executable = (string) $arguments->first();
+            try {
+                $parser = (new ParserFactory())->create(ParserFactory::ONLY_PHP7);
+                $parser->parse((string) file_get_contents($executable));
+                $array = $arguments->toArray();
+                array_unshift($array, $this->executable);
+                $arguments = new ProcessArgumentsCollection($array);
+            } catch (\Throwable) {
+                // Parser failed, not a PHP script
+            }
         }
 
         return parent::buildProcess($arguments);
